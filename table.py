69c8efed453e134c2a6ca445a268f6994b3ca917
chandrushal <115072149+chandrushal@users.noreply.github.com>
2024-04-24 15:26:16 +0530

    Update table.py

--- table.py
+++ table.py
@@ -16,50 +16,40 @@ CREATE TABLE IF NOT EXISTS detail (
 # print("Table created successfully")
 
 #Insert a row into the 'detail' table
-        insert_query = """
-        INSERT INTO detail (id, name, date_of_joining) 
-        VALUES (5, 'siva', '2024-04-21')
-        """
-        cursor.execute(insert_query)
-        sqliteConnection.commit()  # Commit the transaction
-        
-        select_query = "SELECT * FROM detail"
-        
-        cursor.execute(select_query)
-        rows = cursor.fetchall()
-         # Print the rows
-        for row in rows:
-            print(row)
-        print("Data inserted successfully.")
+insert_query = """
+INSERT INTO detail (id, name, date_of_joining) 
+VALUES (5, 'siva', '2024-04-21')
+"""
+cursor.execute(insert_query)
+print("Data inserted successfully.")
 
 #Update rows in the 'details' table
-            update_query = """
-            UPDATE detail 
-            SET name = 'kumar', date_of_joining = '2024-05-01' 
-            WHERE id = 1
-            """
-            cursor.execute(update_query)
-            sqliteConnection.commit()  # Commit the transaction
-            # select_query = "SELECT * FROM detail"
-
-            # cursor.execute(select_query)
-            # rows = cursor.fetchall()
-            #  # Print the rows
-            # for row in rows:
-            #     print(row)
-            # print("Data updated successfully.")
+update_query = """
+UPDATE detail 
+SET name = 'kumar', date_of_joining = '2024-05-01' WHERE id = 1
+"""
+cursor.execute(update_query)
+sqliteConnection.commit()  # Commit the transaction
+print("Data updated successfully.")
 # delete query and view the table
-            delete_query = "DELETE FROM detail WHERE id = 1"
-            cursor.execute(delete_query)
-            sqliteConnection.commit()  # Commit the transaction
-            select_query = "SELECT * FROM detail"
+delete_query = "DELETE FROM detail WHERE id = 1"
+cursor.execute(delete_query)
 
-            cursor.execute(select_query)
-            rows = cursor.fetchall()
-             # Print the rows
-             for row in rows:
-                     print(row)
-            print("Data deleted successfully.")
+# drop_query
+ drop_query = "DROP TABLE IF EXISTS detail"
+cursor.execute(drop_query)
+
+#truncate
+truncate_query = "DELETE FROM detail"
+cursor.execute(truncate_query)
+
+select_query = "SELECT * FROM detail"
+cursor.execute(select_query)
+rows = cursor.fetchall()
+# Print the rows
+for row in rows:
+print(row)
+print("Data deleted successfully.")
 # Close the cursor and the connection
 cursor.close()
 sqliteConnection.close()
